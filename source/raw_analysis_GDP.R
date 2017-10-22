902999db1553ca2354bea82e9f9ae8d92de92f09
JaimeVillanueva <31341581+JaimeVillanueva@users.noreply.github.com>
2017-10-22 10:14:41 -0400

    Update raw_analysis_GDP.R
    Many changes: highlights:
    1. added necessary libraries
    2. forced read in of file to fill all blanks with NA
    3. moved filtering out of NA before conversion
    4. changed short code column name to match edstats_final to facilitate merge
    5. removed commas from million of US dollars, this is why conversion to numeric wouldn't work

--- source/raw_analysis_GDP.R
+++ source/raw_analysis_GDP.R
@@ -2,12 +2,15 @@
 # Jaime Villanueva
 # 10/21/2017
 
+library(dyplr)
+library(tidyr)
+
 # Read in and analyze the header to check how many rows to skip for a potential re-read
 # Working directory needs to be directory containing .csv to read in the data
 gdp_2012 <- read.csv("6306 case study getdata_data_GDP.csv")
 tbl_df(gdp_2012)                              # create data frame and look at first 10 lines
 
-gdp_2012 <- read.csv("6306 case study getdata_data_GDP.csv", skip=4, stringsAsFactors = FALSE)
+gdp_2012 <- read.csv("6306 case study getdata_data_GDP.csv", skip=4, stringsAsFactors = FALSE, na.strings = c("", "NA"))
 
 dfgdp_2012 <- tbl_df(gdp_2012)                # create data frame and look at the first 10 lines
 sapply(dfgdp_2012, function(x) all(is.na(x))) # check for columns with no data
@@ -17,12 +20,18 @@ dfgdp_2012$X.5                                # displaying data for X.5
 
 # X.5 data doesn't make sense, analysis of raw data shows it is not data but notations
 # select and rename columns 1,2,4, and 5
-dfgdp_2012_clean1 <- select(dfgdp_2012, "Country Short Code" = X, Ranking = X.1, Country = X.3, "Millions of US Dollars" = X.4)
+dfgdp_2012_clean1 <- select(dfgdp_2012, "CountryCode" = X, Ranking = X.1, Country = X.3, "Millions of US Dollars" = X.4)
 
+# filter out any line without a country code and ranking
+dfgdp_2012_clean2 <- filter(dfgdp_2012_clean1, !is.na(Ranking) & !is.na(CountryCode))
+       
+# get rid of the commas in the "Millions of US Dollars" column
+dfgdp_2012_clean2$"Millions of US Dollars" <- gsub(",", "", dfgdp_2012_clean2$"Millions of US Dollars")
                               
-dfgdp_2012_clean1$Ranking <- as.numeric(dfgdp_2012_clean1$Ranking)
-#dfgdp_2012_clean1$"Millions of US Dollars" <- as.numeric(dfgdp_2012_clean1$"Millions of US Dollars")
-dfgdp_2012_final <- filter(dfgdp_2012_clean1, !is.na(Ranking))
-dfgdp_2012_final
+# convert Ranking and Millions of US Dollars columns to numeric
+dfgdp_2012_clean2$Ranking <- as.numeric(dfgdp_2012_clean2$Ranking)
+dfgdp_2012_clean2$"Millions of US Dollars" <- as.numeric(dfgdp_2012_clean2$"Millions of US Dollars")
+
+dfgdp_2012_final <- dfgdp_2012_clean2
 
 
