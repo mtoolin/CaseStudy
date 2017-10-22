fa072448bdf853e25c3891920922d6e1f9ce3cc5
mtoolin <mtoolin@mail.smu.edu>
2017-10-22 11:57:30 -0500

    Initial check in.  This file downloads the two data files gdp.csv and edstats_country.csv from the Internet and stores them in the data directory

---
+++ source/ReadURLData.R
@@ -0,0 +1,16 @@
+# Group ProjectCase Study for MSDS 6306
+# Revised on October 22, 2017
+# Author: Michael Toolin
+
+#
+# Download the required tables from URL's given in the problem statement
+# and save them in the data directory
+#
+
+URL1 <-"https://d396qusza40orc.cloudfront.net/getdata/data/GDP.csv"
+URL2 <-"https://d396qusza40orc.cloudfront.net/getdata/data/EDSTATS_Country.csv"
+destfile1 <-"data/gdp.csv"
+destfile2 <-"data/edstats_country.csv"
+
+download.file(URL1,destfile1)
+download.file(URL2,destfile2)
