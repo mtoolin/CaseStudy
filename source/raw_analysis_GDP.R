832008ccf55c28f930cc4c6b19ece86a66c5fa6c
JaimeVillanueva <31341581+JaimeVillanueva@users.noreply.github.com>
2017-10-22 22:16:21 -0400

    Update raw_analysis_GDP.R

--- source/raw_analysis_GDP.R
+++ source/raw_analysis_GDP.R
@@ -1,5 +1,5 @@
 # Raw Analysis for GDP Data
-# Input: GDP Data from https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv
+# Input: gdp.csv
 # Output: dfgdp_2012_final.csv
 
 library(dplyr)
@@ -7,10 +7,10 @@ library(tidyr)
 
 # Read in and analyze the header to check how many rows to skip for a potential re-read
 # Working directory needs to be directory containing .csv to read in the data
-gdp_2012 <- read.csv("6306 case study getdata_data_GDP.csv")
+gdp_2012 <- read.csv("data/gdp.csv")
 tbl_df(gdp_2012)                              # create data frame and look at first 10 lines
 
-gdp_2012 <- read.csv("6306 case study getdata_data_GDP.csv", skip=4, stringsAsFactors = FALSE, na.strings = c("", "NA"))
+gdp_2012 <- read.csv("data/gdp.csv", skip=4, na.strings = c("","NA"), stringsAsFactors = FALSE)
 
 dfgdp_2012 <- tbl_df(gdp_2012)                # create data frame and look at the first 10 lines
 sapply(dfgdp_2012, function(x) all(is.na(x))) # check for columns with no data
@@ -19,22 +19,23 @@ sapply(dfgdp_2012, function(x) all(is.na(x))) # check for columns with no data
 dfgdp_2012$X.5                                # displaying data for X.5
 
 # X.5 data doesn't make sense, analysis of raw data shows it is not data but notations
+
 # select and rename columns 1,2,4, and 5
-dfgdp_2012_clean1 <- select(dfgdp_2012, "CountryCode" = X, Ranking = X.1, Country = X.3, "Millions of US Dollars" = X.4)
+dfgdp_2012_clean1 <- select(dfgdp_2012, "CountryCode" = X, Ranking = X.1, Country = X.3, "Millions.US.Dollars" = X.4)
 
 # filter out any line without a country code and ranking
 dfgdp_2012_clean2 <- filter(dfgdp_2012_clean1, !is.na(Ranking) & !is.na(CountryCode))
-       
-# get rid of the commas in the "Millions of US Dollars" column
-dfgdp_2012_clean2$"Millions of US Dollars" <- gsub(",", "", dfgdp_2012_clean2$"Millions of US Dollars")
+
+# get rid of the commas in the "Millions.US.Dollars" column
+dfgdp_2012_clean2$"Millions.US.Dollars" <- gsub(",", "", dfgdp_2012_clean2$"Millions.US.Dollars")
                               
-# convert Ranking and Millions of US Dollars columns to numeric
+# convert Ranking and Millions.US.Dollars columns to numeric
 dfgdp_2012_clean2$Ranking <- as.numeric(dfgdp_2012_clean2$Ranking)
-dfgdp_2012_clean2$"Millions of US Dollars" <- as.numeric(dfgdp_2012_clean2$"Millions of US Dollars")
+dfgdp_2012_clean2$"Millions.US.Dollars" <- as.numeric(dfgdp_2012_clean2$"Millions.US.Dollars")
 
 dfgdp_2012_final <- dfgdp_2012_clean2
-       
+
 # Working directory is Data folder
-write.csv(dfgdp_2012_final, "dfgdp_2012_final.csv", row.names = FALSE)
+write.csv(dfgdp_2012_final, "data/dfgdp_2012_final.csv", row.names = FALSE)
 
 
