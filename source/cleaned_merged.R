84ac3b2a8e67a35c033522bfad792f43a213821b
JaimeVillanueva <31341581+JaimeVillanueva@users.noreply.github.com>
2017-10-22 22:31:36 -0400

    Update cleaned_merged.R

--- source/cleaned_merged.R
+++ source/cleaned_merged.R
@@ -2,8 +2,8 @@
 # Input: merged_final.csv
 # Ouput: cleaned_merged_final.csv
 
-merged_final <- read.csv("merged_final.csv", stringsAsFactors = FALSE)
+merged_final <- read.csv("data/merged_final.csv", stringsAsFactors = FALSE)
 
 cleaned_merged_final <- select(merged_final, "CountryCode", "Country", "Ranking", "Millions of US Dollars" = "Millions.of.US.Dollars", "Income Group" = "Income.Group")
 
-write.csv(cleaned_merged_final, "cleaned_merged_final.csv", row.names = FALSE)
+write.csv(cleaned_merged_final, "data/cleaned_merged_final.csv", row.names = FALSE)
