7b4ea987ac8cda462b51e987daabb06d9eae255f
JaimeVillanueva <31341581+JaimeVillanueva@users.noreply.github.com>
2017-10-27 04:42:00 -0400

    Update raw_analysis_EDSTATS.R

--- source/raw_analysis_EDSTATS.R
+++ source/raw_analysis_EDSTATS.R
@@ -4,21 +4,20 @@
 
 library(dplyr)
 
-# working directory needs to be directory containing .csv to read in the data
 edstats <- read.csv("data/edstats_country.csv", na.strings = c("","NA"), stringsAsFactors = FALSE)
 dfgdp_edstats <- read.csv("data/dfgdp_2012_final.csv", stringsAsFactors = FALSE)
 
 edstats <- tbl_df(edstats)
-dfgdp_edstats <- tbl_df(dfgdp_edstats)    # Needed for country code comparison vector to get rid of countries no
+dfgdp_edstats <- tbl_df(dfgdp_edstats)    # Needed for country code comparison vector to get rid of countries no GDP data
 
-matched.result <- match(edstats$CountryCode, dfgdp_edstats$CountryCode)     # Creates vector holding line number in edstats of match
-
+matched.result <- match(edstats$CountryCode, dfgdp_edstats$CountryCode)     # Creates vector holding line number 
+                                                                            # in edstats of match
 edstats_final <- data.frame(stringsAsFactors = FALSE)     #initialize empty data frame
 edstats_final <- tbl_df(edstats_final)
-for (x in 1:length(matched.result)){
-  ifelse (is.na(matched.result[x]),"NA" , edstats_final <-rbind(edstats_final, edstats[x,]))
-}
-
+for (x in 1:length(matched.result)){                                                          # Iterate through matched.result
+  ifelse (is.na(matched.result[x]),"NA" , edstats_final <-rbind(edstats_final, edstats[x,]))  # checking for NA. If not NA
+}                                                                                             # If not NA, whole row add to
+                                                                                              # end of edstats_final
 # Working directory is Data folder
 write.csv(edstats_final, "data/edstats_final.csv", row.names = FALSE)
 
